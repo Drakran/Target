18d9eb42cc5c06ef967332727d5d003dce7d8bef
Terry Wang <terrywangce@gmail.com>
2017-09-28 21:13:27 -0500

    I actually don't know what changes i made

--- src/MotorTest.java
+++ src/MotorTest.java
@@ -7,9 +7,9 @@ public class MotorTest {
 		RXTXRobot r = new ArduinoNano();
 		r.setPort("COM5");
 		r.connect();
-		//r.runMotor(RXTXRobot.MOTOR1, 225, 3000); //O time = infite 0 speed = stop
+		r.runMotor(RXTXRobot.MOTOR1, 225, 3000); //O time = infite 0 speed = stop
 		//r.runMotor(RXTXRobot.MOTOR1, 0, 0); // speed, time
-		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100);
+		//r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100);
 		r.close();
 	}
 
