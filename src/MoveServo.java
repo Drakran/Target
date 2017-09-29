9981c7ea71a762bcf488cfa158f29afa38d27c0e
Terry Wang <terrywangce@gmail.com>
2017-09-29 18:40:46 -0500

    Something Happened

--- src/MoveServo.java
+++ src/MoveServo.java
@@ -9,7 +9,7 @@ public class MoveServo
 		r.setPort("COM5"); // Set the port to COM3 
 		//r.setVerbose(true); // Turn on debugging messages 
 		r.connect(); 
-//		r.attachServo(RXTXRobot.SERVO1, 9); //Connect the servos to the Arduino 
+		r.attachServo(RXTXRobot.SERVO1, 9); //Connect the servos to the Arduino 
 //		r.attachServo(RXTXRobot.SERVO2, 10); 
 		r.moveServo(RXTXRobot.SERVO1, 90); // Move Servo 1 to location 30 
 		//r.moveServo(RXTXRobot.SERVO2, 170); // Move Servo 2 to location 170 
