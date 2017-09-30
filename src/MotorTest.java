d6edc4f1f4bba045039cce2c66702540264b3774
Terry Wang <terrywangce@gmail.com>
2017-09-30 15:27:36 -0500

    The thing is not working

--- src/MotorTest.java
+++ src/MotorTest.java
@@ -7,9 +7,11 @@ public class MotorTest {
 		RXTXRobot r = new ArduinoNano();
 		r.setPort("COM5");
 		r.connect();
+		r.setVerbose(true);
 		//r.runMotor(RXTXRobot.MOTOR1, 225, RXTXRobot.MOTOR2, 500, 300); //O time = infite 0 speed = stop
-		r.runMotor(RXTXRobot.MOTOR1, 225, 300); // speed, time
-		//r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100);
+		r.attachMotor(RXTXRobot.MOTOR1, 5);
+		r.runMotor(RXTXRobot.MOTOR1, 300, 3000); // speed, time
+//		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100);
 		r.close();
 		
 	}
