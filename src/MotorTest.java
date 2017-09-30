92211cdf69a3cefd6bd9b2e128f3805f5df3100c
Terry Wang <terrywangce@gmail.com>
2017-09-30 18:11:32 -0500

    We're trying

--- src/MotorTest.java
+++ src/MotorTest.java
@@ -8,8 +8,9 @@ public class MotorTest {
 		r.setPort("COM5");
 		r.connect();
 		r.setVerbose(true);
+		r.refreshDigitalPins();
 		//r.runMotor(RXTXRobot.MOTOR1, 225, RXTXRobot.MOTOR2, 500, 300); //O time = infite 0 speed = stop
-//		r.attachMotor(RXTXRobot.MOTOR1, 5);
+		r.attachMotor(RXTXRobot.MOTOR1, 5);
 		r.runMotor(RXTXRobot.MOTOR1, 300, 3000); // speed, time
 //		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100);
 		r.close();
