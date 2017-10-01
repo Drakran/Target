fa3ec6ae0e1078bfc6c835c0fd3339124098f4ef
Terry Wang <terrywangce@gmail.com>
2017-10-01 15:49:19 -0500

    Eh

--- src/MotorTest.java
+++ src/MotorTest.java
@@ -5,14 +5,16 @@ public class MotorTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RXTXRobot r = new ArduinoNano();
+		//RXTXRobot test = new RXTXRobot();
 		r.setPort("COM5");
 		r.connect();
 		r.setVerbose(true);
 		//r.refreshDigitalPins();
 		//r.runMotor(RXTXRobot.MOTOR1, 225, RXTXRobot.MOTOR2, 500, 300); //O time = infite 0 speed = stop
-		r.attachMotor(RXTXRobot.MOTOR2, 6);
+		r.attachMotor(RXTXRobot.MOTOR1, 5);
 //		r.attachMotor(RXTXRobot.MOTOR2, 6);
-		r.runMotor(RXTXRobot.MOTOR2, 200, 3000);
+		r.runMotor(RXTXRobot.MOTOR1, 500, 3000);
+//		r.runMotor(RXTXRobot.MOTOR2, 200, 3000);
 //		r.runMotor(RXTXRobot.MOTOR1, 500, RXTXRobot.MOTOR2, 500, 0); // speed, time
 //		r.runEncodedMotor(RXTXRobot.MOTOR1, 500, 10);
 
