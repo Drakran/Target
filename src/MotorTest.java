56fa6005eb0315c9c54144e59d155494a2c14e71
Terry Wang <terrywangce@gmail.com>
2017-10-01 14:57:21 -0500

    Why is this not working

--- src/MotorTest.java
+++ src/MotorTest.java
@@ -7,12 +7,15 @@ public class MotorTest {
 		RXTXRobot r = new ArduinoNano();
 		r.setPort("COM5");
 		r.connect();
-		//r.setVerbose(true);
+		r.setVerbose(true);
 		//r.refreshDigitalPins();
 		//r.runMotor(RXTXRobot.MOTOR1, 225, RXTXRobot.MOTOR2, 500, 300); //O time = infite 0 speed = stop
 		r.attachMotor(RXTXRobot.MOTOR2, 6);
-		r.runMotor(RXTXRobot.MOTOR2, 300, 3000); // speed, time
-//		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 100);
+//		r.attachMotor(RXTXRobot.MOTOR2, 6);
+		r.runMotor(RXTXRobot.MOTOR2, 200, 3000);
+//		r.runMotor(RXTXRobot.MOTOR1, 500, RXTXRobot.MOTOR2, 500, 0); // speed, time
+//		r.runEncodedMotor(RXTXRobot.MOTOR1, 500, 10);
+
 		r.close();
 		
 	}
--- src/MoveServo.java
+++ src/MoveServo.java
@@ -9,11 +9,15 @@ public class MoveServo
 		r.setPort("COM5"); // Set the port to COM3 
 		r.setVerbose(true); // Turn on debugging messages 
 		r.connect(); 
-		r.refreshDigitalPins();
+		int num = 180;
+		int reverse = 360 -num;
+		//r.refreshDigitalPins();
 		r.attachServo(RXTXRobot.SERVO1, 9); //Connect the servos to the Arduino 
+		r.moveServo(RXTXRobot.SERVO1, num); // Move Servo 1 to location 30   //360
+		r.moveServo(RXTXRobot.SERVO1, reverse);
 //		r.attachServo(RXTXRobot.SERVO2, 10); 
-		r.moveServo(RXTXRobot.SERVO1, 180); // Move Servo 1 to location 30 
-		//r.moveServo(RXTXRobot.SERVO2, 170); // Move Servo 2 to location 170 
+//		r.moveServo(RXTXRobot.SERVO2, 90); // Move Servo 2 to location 170  //180
+//		r.moveServo(RXTXRobot.SERVO2, 0);
 		r.close(); 
 	} 
 } 
