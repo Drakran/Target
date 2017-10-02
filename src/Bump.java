6665bf64b4a28d46925039b6861ba987f5d0ce30
Terry Wang <terrywangce@gmail.com>
2017-10-02 16:08:10 -0500

    Hopefully this works

---
+++ src/Bump.java
@@ -0,0 +1,37 @@
+import rxtxrobot.ArduinoNano;
+import rxtxrobot.*;
+
+
+public class Bump {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RXTXRobot r = new ArduinoNano(); // Create RXTXRobot object 
+		r.setPort("COM5"); // Set the port to COM3 
+		r.connect(); 
+
+//		for(int x =0; x < times; x++) {
+//			value = r.getDigitalPin(5).getValue(); //0 is non touch 1 is touch
+//			System.out.println("Value: " + value + "\n");
+//		}
+		//r.refreshDigitalPins();
+//		while((r.getDigitalPin(4).getValue()) == 0) {
+//			//r.refreshDigitalPins();
+//			System.out.println("Value: " + (r.getDigitalPin(4).getValue()) + "\n");
+//			r.sleep(100);
+//		}
+		r.refreshAnalogPins();
+		while(true) {
+			r.refreshAnalogPins();
+			int temp = r.getAnalogPin(4).getValue();
+			System.out.println(temp);
+			if(temp <= 10) {
+				System.out.println("Something");
+				break;
+			}
+		}
+		r.close();
+
+	}
+
+}
---
+++ src/Ping.java
@@ -0,0 +1,26 @@
+import rxtxrobot.ArduinoNano;
+import rxtxrobot.RXTXRobot;
+import rxtxrobot.*;
+
+
+
+
+
+public class Ping {
+
+	final private static int PING_PIN = 12; 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RXTXRobot r = new ArduinoNano(); // Create RXTXRobot object 
+		r.setPort("COM5"); // Set the port to COM3 
+		r.connect(); 
+		int times = 10;
+		for(int x =0; x < times; x++) {
+			System.out.println("Response: " + r.getPing(PING_PIN) + " cm"); 
+			r.sleep(300); 
+		}
+		r.close();
+	}
+
+}
