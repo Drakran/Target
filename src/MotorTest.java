1d10414170f6226c51f6314818c1346a4a0bb8ca
Terry Wang <terrywangce@gmail.com>
2017-10-02 18:08:08 -0500

    It's Alive!

--- src/MotorTest.java
+++ src/MotorTest.java
@@ -15,14 +15,24 @@ public class MotorTest {
 		//r.refreshDigitalPins();
 		//r.runMotor(RXTXRobot.MOTOR1, 225, RXTXRobot.MOTOR2, 500, 300); //O time = infite 0 speed = stop
 		r.attachMotor(right, 5);
+		r.attachMotor(left, 6);
 		//System.out.println(right);
 //		r.attachMotor(RXTXRobot.MOTOR2, 6);
-		r.runMotor(right, 500, 3000);
+//		r.runMotor(right, 500, 1000);
 //		r.runMotor(left, 200, 3000);
-//		r.runMotor(RXTXRobot.MOTOR1, 500, RXTXRobot.MOTOR2, 500, 0); // speed, time
-//		r.runEncodedMotor(RXTXRobot.MOTOR1, 500, 10);
+		r.runMotor(right, -100, left, 120, 0); 
+		while(r.getAnalogPin(4).getValue() > 0) {
+			r.refreshAnalogPins();
 
-		r.close();
+			if(r.getAnalogPin(4).getValue() == 0) {
+				r.close();
+				break;
+			}
+		}
+//		r.runMotor(right, -100, left, 100, 0); // speed, time
+//		r.runEncodedMotor(RXTXRobot.MOTOR1, 500, 10);
+		
+//		r.close();
 		
 	}
 
