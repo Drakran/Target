b88f35f72bfb8cedbae9b26be5df2c8de060f98f
Terry Wang <terrywangce@gmail.com>
2017-10-02 15:40:37 -0500

    Works Now

--- src/MotorTest.java
+++ src/MotorTest.java
@@ -2,6 +2,9 @@ import rxtxrobot.*;
 
 public class MotorTest {
 
+	public static int right = RXTXRobot.MOTOR1;
+	public static int left = RXTXRobot.MOTOR2;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RXTXRobot r = new ArduinoNano();
@@ -11,10 +14,11 @@ public class MotorTest {
 		r.setVerbose(true);
 		//r.refreshDigitalPins();
 		//r.runMotor(RXTXRobot.MOTOR1, 225, RXTXRobot.MOTOR2, 500, 300); //O time = infite 0 speed = stop
-		r.attachMotor(RXTXRobot.MOTOR1, 5);
+		r.attachMotor(right, 5);
+		//System.out.println(right);
 //		r.attachMotor(RXTXRobot.MOTOR2, 6);
-		r.runMotor(RXTXRobot.MOTOR1, 500, 3000);
-//		r.runMotor(RXTXRobot.MOTOR2, 200, 3000);
+		r.runMotor(right, 500, 3000);
+//		r.runMotor(left, 200, 3000);
 //		r.runMotor(RXTXRobot.MOTOR1, 500, RXTXRobot.MOTOR2, 500, 0); // speed, time
 //		r.runEncodedMotor(RXTXRobot.MOTOR1, 500, 10);
 
--- src/MoveServo.java
+++ src/MoveServo.java
@@ -10,15 +10,17 @@ public class MoveServo
 		r.setVerbose(true); // Turn on debugging messages 
 		r.connect(); 
 		int num = 180;
-		int reverse = 360 -num;
+		//int reverse = 360 -num;
 		//r.refreshDigitalPins();
-		r.attachServo(RXTXRobot.SERVO1, 9); //Connect the servos to the Arduino 
-		r.moveServo(RXTXRobot.SERVO1, num); // Move Servo 1 to location 30   //360
-		r.moveServo(RXTXRobot.SERVO1, reverse);
-//		r.attachServo(RXTXRobot.SERVO2, 10); 
-//		r.moveServo(RXTXRobot.SERVO2, 90); // Move Servo 2 to location 170  //180
-//		r.moveServo(RXTXRobot.SERVO2, 0);
-		r.close(); 
+//		r.attachServo(RXTXRobot.SERVO1, 9);
+//		r.moveServo(RXTXRobot.SERVO1, 180); //Connect the servos to the Arduino 
+//		r.moveServo(RXTXRobot.SERVO1, -90); // Move Servo 1 to location 30   //360
+//		r.moveServo(RXTXRobot.SERVO1, reverse);
+		r.attachServo(RXTXRobot.SERVO2, 10); 
+		r.moveServo(RXTXRobot.SERVO2, 71); // Move Servo 2 to location 170  //180
+		r.sleep(1000);
+		r.moveServo(RXTXRobot.SERVO2, 180);
+		//r.close(); 
 	} 
 } 
 //Hi
